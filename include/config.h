2bc629cb53311dd091abbda21abd3e09759b78b6
denstiny <2254228017@qq.com>
2021-01-19 00:44:23 +0800

    file

--- include/config.h
+++ include/config.h
@@ -8,7 +8,7 @@
 #define EKHOCOFIG false
 #define VOIDO "playout"
 #define VOIDOCONFIG true
-#define VOIDOFILE "/mnt/home/todo-list/src/bang.mp3"
+#define VOIDOFILE "/mnt/home/todo-list/src/material/keli.mp3"
 #define SYS system
 typedef std::string Str;
 typedef struct Todo
@@ -29,26 +29,6 @@ typedef struct date
 		s;
 }Da;
 
-#define Mon 1
-#define Tue 2
-#define Wed 3
-#define Thu 4
-#define Fri 5
-#define Sat 6
-#define Sun 7
-
-#define Jan 1
-#define Feb 2
-#define Mar 3
-#define Apr 4
-#define May 5
-#define Jun 6
-#define Jul 7
-#define Aug 8
-#define Sep 9
-#define Oct 10
-#define Nov 11
-#define Dec 12
 const std::string MONTH[12]={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
 
 #endif
